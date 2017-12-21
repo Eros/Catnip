e5546b3fc58972f264447df92bd7d3c3b4273321
Eros <george.marr.cobc@gmail.com>
2017-12-21 21:35:35 +0000

    IT TOOK ME 15 MINUTES TO FIGURE OUT IT WAS A FUCKING PARENTHESES

--- src/main/java/io/gpm/FileUploadEvent.java
+++ src/main/java/io/gpm/FileUploadEvent.java
@@ -7,7 +7,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-import java.io.IOException;
+import java.io.*;
+import java.util.logging.Level;
 
 /***
  * @author George
@@ -19,16 +20,36 @@ public class FileUploadEvent {
 
     protected static void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
-        response.setContentType("");//replace with image
+        response.setContentType("");//todo replace with image
 
         //general setting up shit
-
         final String path = ""; //replace with the path
         final Part filePart = request.getPart("file");
+        final String fileName = getFileName(filePart);
+
+        //streams and shit
+        OutputStream out = null;
+        InputStream in = null;
+        PrintWriter writer = response.getWriter();
+
+        try {
+            out = new FileOutputStream(new File(path + File.separator + fileName));
+            in = filePart.getInputStream();
+
+            int read = 0;
+            final byte[] bytes = new byte[1024];
+
+            while((read = in.read(bytes)) != -1){
+                out.write(bytes, 0, read);
+            }
+
+        } catch (FileNotFoundException e){
+            log.info(Level.SEVERE + "Exception caught: " + e);
+        }
 
     }
 
-    private static String getFleName(final Part part){
+    private static String getFileName(final Part part){
         final String partHeader = part.getHeader("content-disposition");
         log.info("Part header = {0}", partHeader.intern());
 
