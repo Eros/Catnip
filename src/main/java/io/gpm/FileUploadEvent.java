bcb2a1ea76639f1d1f02d8bff441c3550761beb9
Eros <george.marr.cobc@gmail.com>
2017-12-21 21:28:12 +0000

    fuck me that gave me ball ache

--- src/main/java/io/gpm/FileUploadEvent.java
+++ src/main/java/io/gpm/FileUploadEvent.java
@@ -1,5 +1,8 @@
 package io.gpm;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +15,7 @@ import java.io.IOException;
  */
 public class FileUploadEvent {
 
+    private static final Logger log = LoggerFactory.getLogger("[Catnip-Log] ");
 
     protected static void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
@@ -22,5 +26,18 @@ public class FileUploadEvent {
         final String path = ""; //replace with the path
         final Part filePart = request.getPart("file");
 
+    }
+
+    private static String getFleName(final Part part){
+        final String partHeader = part.getHeader("content-disposition");
+        log.info("Part header = {0}", partHeader.intern());
+
+        for(String c : partHeader.split(";")){
+            if(c.trim().startsWith("fileName")){
+                return c.substring(
+                        c.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return null;
     }
 }
