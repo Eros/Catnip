c0d3de831f42313f56da181e0ec22aae01e91d87
Eros <george.marr.cobc@gmail.com>
2017-12-21 21:05:06 +0000

    init

---
+++ src/main/java/io/gpm/Catnip.java
@@ -0,0 +1,66 @@
+package io.gpm;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.event.Event;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/***
+ * @author George
+ * @since 21-Dec-17
+ */
+public class Catnip {
+
+    //general bollocks for twitter
+    public static final int TPS_STREAM_LENGTH = 1000;
+
+
+    public static void main(String... args){
+        //queing for the shit we send
+        BlockingQueue<String> messageQueue = new LinkedBlockingQueue<String>(TPS_STREAM_LENGTH);
+        BlockingQueue<Event> eventQueue = new LinkedBlockingQueue<Event>(TPS_STREAM_LENGTH);
+
+        //hosts and crap
+        Hosts host = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
+
+        //adds the terms (may not be needed but fuck it)
+        List<String> terms = Lists.newArrayList("twitter", "cats", "cute cats");
+
+        //authentication
+        Authentication auth = new OAuth1("consumerKey", "consumerSecret", "token", "secret");
+
+        //client handling
+        ClientBuilder builder = new ClientBuilder()
+                .name("catnip-twitter-system")
+                .hosts(host)
+                .authentication(auth)
+                .endpoint(endpoint)
+                .processor(new StringDelimitedProcessor(messageQueue))
+                .eventMessageQueue(eventQueue);
+
+        Client client = builder.build();
+
+        client.connect();
+
+        while(!client.isDone()){
+            //this is where we do shit
+        }
+        if(client.isDone()) {
+            client.stop();
+            System.out.println("Client: " + client.getName() + " has been stopped. Reason: Finished.");
+        }
+    }
+}
---
+++ src/main/java/io/gpm/Credentials.java
@@ -0,0 +1,8 @@
+package io.gpm;
+
+/***
+ * @author George
+ * @since 21-Dec-17
+ */
+public class Credentials {
+}
