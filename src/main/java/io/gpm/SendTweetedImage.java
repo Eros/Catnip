f5ff82dfc09e5710682e1020aa1f1c48416189ab
Eros <george.marr.cobc@gmail.com>
2017-12-31 20:48:19 +0000

    fuck

--- src/main/java/io/gpm/SendTweetedImage.java
+++ src/main/java/io/gpm/SendTweetedImage.java
@@ -1,8 +1,21 @@
 package io.gpm;
 
+import twitter4j.Twitter;
+
 /***
  * @author George
  * @since 31-Dec-17
  */
 public class SendTweetedImage {
+
+    public static void go(String... args){
+        if(args.length < 1){
+            System.out.println("Error sending multi media tweet!");
+        }
+        try {
+            Twitter twitter = Catnip.twitter;
+
+            long[] mediaID = new long[args.length - 1];
+        }
+    }
 }
