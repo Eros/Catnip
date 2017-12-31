06641d019ebaf8c63ea6d46fbaf67dcbda18cc85
Eros <george.marr.cobc@gmail.com>
2017-12-31 20:51:33 +0000

    added logging

--- src/main/java/io/gpm/SendTweetedImage.java
+++ src/main/java/io/gpm/SendTweetedImage.java
@@ -1,6 +1,10 @@
 package io.gpm;
 
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.UploadedMedia;
+
+import java.io.File;
 
 /***
  * @author George
@@ -16,6 +20,20 @@ public class SendTweetedImage {
             Twitter twitter = Catnip.twitter;
 
             long[] mediaID = new long[args.length - 1];
+            for(int i = 1; i < args.length; i++) {
+                System.out.println("Uploading image: " + i + "/" + (args.length - 1) + "] [" + args[i]  + "]");
+                UploadedMedia media = twitter.uploadMedia(new File(args[i]));
+                System.out.println("Uploaded image!\n " +
+                        "Image information: \n" +
+                        "ID:" + media.getMediaId()
+                 + "Width: " + media.getImageWidth()
+                + "Height: " + media.getImageHeight()
+                + "Size: " + media.getSize()
+                + "Format: " + media.getSize());
+
+            }
+        } catch (TwitterException e) {
+            e.printStackTrace();
         }
     }
 }
