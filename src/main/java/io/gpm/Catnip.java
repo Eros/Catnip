c4819e0c77a2b4bd9aff89c4668062e23ec4063b
Eros <george.marr.cobc@gmail.com>
2017-12-31 16:23:27 +0000

    fuck it this is gonna be fun

--- src/main/java/io/gpm/Catnip.java
+++ src/main/java/io/gpm/Catnip.java
@@ -1,14 +1,24 @@
 package io.gpm;
 
 
+import twitter4j.*;
+
+import java.io.IOException;
+
 /***
  * @author George
  * @since 21-Dec-17
  */
 public class Catnip {
 
+
     public static void main(String... args) {
-
-
+        try {
+            Twitter twitter = new TwitterFactory().getInstance();
+            User user = twitter.verifyCredentials();
+            System.out.println("Verified credentials of user: " + user.getName());
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
     }
 }
