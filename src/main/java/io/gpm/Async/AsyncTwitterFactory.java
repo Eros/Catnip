113de92366cc395f40cfb86372d7e9874afe8b38
Eros <george.marr.cobc@gmail.com>
2018-01-02 09:46:43 +0000

    added more shit

---
+++ src/main/java/io/gpm/Async/AsyncTwitterFactory.java
@@ -0,0 +1,20 @@
+package io.gpm.Async;
+
+import twitter4j.conf.Configuration;
+
+import java.io.Serializable;
+
+/***
+ * @author George
+ * @since 02/01/2018
+ */
+public class AsyncTwitterFactory implements Serializable {
+
+    private final Configuration conf;
+    private static final AsyncTwitter SINGLETON;
+
+    static
+    {
+        //impl
+    }
+}
--- src/main/java/io/gpm/AsyncUpdater.java
+++ src/main/java/io/gpm/AsyncUpdater.java
@@ -1,5 +1,7 @@
 package io.gpm;
 
+import twitter4j.Twitter;
+
 /***
  * @author George
  * @since 02/01/2018
@@ -9,6 +11,9 @@ public class AsyncUpdater {
     private static final Object LOCK = new Object();
 
     public static void handle(String... args) throws InterruptedException {
-
+        if(args.length < 1) {
+            System.out.println("Incorrect usage! Aborting...");
+        }
+        Twitter twitter = Catnip.twitter;
     }
 }
