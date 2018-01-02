705171e5719f06ac0c0f3246922bc59a48e70bea
Eros <george.marr.cobc@gmail.com>
2018-01-02 11:14:37 +0000

    k

---
+++ src/main/java/io/gpm/media/ImageUploadFactory.java
@@ -0,0 +1,80 @@
+package io.gpm.media;
+
+import twitter4j.auth.Authorization;
+import twitter4j.auth.AuthorizationFactory;
+import twitter4j.auth.OAuthAuthorization;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationContext;
+
+/***
+ * @author George
+ * @since 02/01/2018
+ */
+public class ImageUploadFactory {
+
+    private final Configuration configuration;
+    private MediaProvider mediaProvider;
+    private final String apiKey;
+
+    public ImageUploadFactory(){
+        this(ConfigurationContext.getInstance());
+    }
+
+    public ImageUploadFactory(Configuration config){
+        String mediaProvider = config.getMediaProvider().toLowerCase();
+        if("twitter".equals(mediaProvider)){
+            //todo default
+        } else if("imgly".equals(mediaProvider) || "img_ly".equals(mediaProvider)){
+            //todo default
+        } else if("twipple".equals(mediaProvider)){
+            //todo default
+        } else {
+            throw new IllegalArgumentException("Unsupported media!!");
+        }
+        this.configuration = config;
+        apiKey = config.getMediaProviderAPIKey();
+    }
+
+    public ImageUpload getInstance(){
+        return getInstance(mediaProvider);
+    }
+
+    public ImageUpload getInstance(Authorization authorization){
+        return getInstance(mediaProvider, authorization);
+    }
+
+    public ImageUpload getInstance(MediaProvider mediaProvider){
+        Authorization auth = AuthorizationFactory.getInstance(configuration);
+        return getInstance(mediaProvider, auth);
+    }
+
+    /**
+     * Returns an ImageUpload instance associated with the specified media provider
+     *
+     * @param mediaProvider media provider
+     * @param authorization authorization
+     * @return ImageUpload
+     * @since Twitter4J 2.1.11
+     */
+
+    public ImageUpload getInstance(MediaProvider mediaProvider, Authorization authorization){
+        if(!(authorization instanceof OAuthAuthorization)){
+            throw new IllegalArgumentException("OAuth is required!");
+        }
+
+        OAuthAuthorization oauth = (OAuthAuthorization) authorization;
+
+        if(mediaProvider == MediaProvider.TWITTER){
+            //todo twitter upload
+        } else if(mediaProvider == MediaProvider.IMG_LY){
+            //todo imgly upload
+        } else if(mediaProvider == MediaProvider.TWIPPLE){
+            //todo twipple upload
+        } else if(mediaProvider == MediaProvider.MOBYPICTURE){
+            //todo mobypicture upload
+        } else {
+            throw new AssertionError("Unknown service provider!");
+        }
+        return (ImageUpload) new AssertionError("");
+    }
+}
---
+++ src/main/java/io/gpm/media/MediaProvider.java
@@ -0,0 +1,14 @@
+package io.gpm.media;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/***
+ * @author George
+ * @since 02/01/2018
+ */
+@AllArgsConstructor @Data
+public enum MediaProvider {
+
+    TWITTER, IMG_LY, TWIPPLE, MOBYPICTURE;
+}
